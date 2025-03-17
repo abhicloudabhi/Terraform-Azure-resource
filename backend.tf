96e22ddd6685de6dcd633ceb72a3659f6f310a1f
abhicloudabhi <abhiabhicloud@gmail.com>
2025-03-17 22:13:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "abhishek-rg"
-    storage_account_name = "deepakstorage4572"
+    resource_group_name  = "deepak-rg"
+    storage_account_name = "deepakstorage4578"
     container_name       = "terraformstorage"
     key                  = "./dev/terraform.tfstate"
     tenant_id            = "var.tenant_id"
